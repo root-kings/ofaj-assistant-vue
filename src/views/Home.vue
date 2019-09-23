43399110cabdadf18170aa938eaa13c8d4ef9c1f
krushndayshmookh <krushndayshmookh@gmail.com>
2019-09-23 11:46:53 +0530

    setup base homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  layout
+    section#documents.section
+      .row
+        .col.s12
+          .card-panel
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Layout from '@/components/Layout'
 
 export default {
-  name: "home",
+  name: 'home',
   components: {
-    HelloWorld
+    Layout
   }
-};
+}
 </script>
