69224680c39db0cff9f85bebae9c964b62cf1f80
krushndayshmookh <krushndayshmookh@gmail.com>
2019-09-23 11:44:47 +0530

    setup layout

---
+++ src/components/Layout.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+  #layout
+    header
+      nav
+        .nav-wrapper.blue-grey
+          a.brand-logo(href="#") OFAJ Assistant
+          a.sidenav-trigger(href="#" data-target="mobile-demo")
+            i.material-icons menu
+          ul#nav-mobile.right.hide-on-med-and-down
+            li
+              router-link(:to="'/'") Home
+            li
+              router-link(:to="'/about'") About
+            li
+              a(@click="logout()") Logout
+      ul#mobile-demo.sidenav
+        li
+          router-link(:to="'/'") Home
+        li
+          router-link(:to="'/about'") About
+        li
+          a(@click="logout()") Logout
+    main
+      slot
+    //- footer
+</template>
+
+<script>
+import M from 'materialize-css'
+
+export default {
+  name: 'Layout',
+  beforeCreate: function() {
+    if (!this.$session.exists() || !this.$session.get('user'))
+      this.$router.push('/login')
+  },
+  mounted: () => {
+    M.AutoInit()
+  },
+  methods: {
+    logout() {
+      this.$session.remove('user')
+      this.$session.destroy()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<!-- styling for the component -->
+<style></style>
