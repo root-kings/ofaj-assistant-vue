956a9a4cc5da368021d89259f04ace1ccba39a32
krushndayshmookh <krushndayshmookh@gmail.com>
2019-09-23 11:39:21 +0530

    ad vue-session materailize and dayhmookh layout

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./registerServiceWorker";
+import Vue from 'vue'
+import VueSession from 'vue-session'
+
+import App from './App.vue'
+import router from './router'
+import './registerServiceWorker'
 import store from './store'
 
-Vue.config.productionTip = false;
+import 'materialize-css'
+import 'materialize-css/dist/css/materialize.css'
+import './assets/css/dayshmookh.layout.css'
+
+Vue.config.productionTip = false
+Vue.use(VueSession)
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
