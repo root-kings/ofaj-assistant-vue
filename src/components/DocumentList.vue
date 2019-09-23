bb6b870cbcc36e2f037d31ae555cd82382e7debc
krushndayshmookh <krushndayshmookh@gmail.com>
2019-09-23 14:38:07 +0530

    document list

---
+++ src/components/DocumentList.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  ul.collapsible#document-list
+    li(v-for="doc in documents" :key="doc._id")
+      .collapsible-header {{ doc.name }}
+      .collapsible-body.white {{ doc.content }}
+</template>
+
+<script>
+export default {
+  props: ['documents']
+}
+</script>
+
+<style scoped lang="scss">
+#document-list {
+  border-radius: 2px;
+  overflow: hidden;
+}
+</style>
